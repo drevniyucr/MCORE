f4bf832f15b9b1aef24d8a8d8c395e6421eace03
maxim akimov <72648180+drevniyucr@users.noreply.github.com>
2025-10-24 18:40:40 +0300

    ----

--- Debug/Core/Src/stm32f7xx_it.d
+++
@@ -1,3 +0,0 @@
-Core/Src/stm32f7xx_it.o: ../Core/Src/stm32f7xx_it.c \
- C:/Users/AkimovMA/Documents/GitHub/MCORE/Core/Inc/stm32f7xx_it.h
-C:/Users/AkimovMA/Documents/GitHub/MCORE/Core/Inc/stm32f7xx_it.h:
